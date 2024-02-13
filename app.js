9db25e8db8e08b35106fcdfac03e9de5fd452ed6
CoddingHyena <Serge49@mail.ru>
2024-02-13 12:28:12 +0300

    init

---
+++ app.js
@@ -0,0 +1,47 @@
+require('@babel/register');
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
+
+const path = require('path');
+
+/// тут будут ручки
+const { checkUser, secureRoute } = require('./src/middlewares/middlewares');
+                                        // у вас может быть другой путь к папке, проверьте
+
+const { PORT } = process.env ?? 3100;
+
+const app = express();
+
+const sessionConfig = {
+  name: 'Cookie',
+  store: new FileStore(),
+  secret: process.env.SESSION_SECRET ?? 'Session',
+  resave: false,
+  saveUninitialized: false, // * если false, куки появятся только при установке req.session
+  cookie: {
+    maxAge: 9999999,
+    httpOnly: true,
+  },
+};
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sessionConfig));
+
+
+/// тут будут app.use
+
+app.get('/*', (req, res) => {
+  res.redirect('/main');
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
---
+++ src/middlewares/middlewares.js
@@ -0,0 +1,23 @@
+const checkUser = (req, res, next) => {
+    if (req.session.login) {
+      next();
+    } else {
+      res.redirect('/log');
+    }
+  };
+  
+  const secureRoute = (req, res, next) => {
+    if (!req.session.login) {
+      next();
+    } else {
+      res.redirect('/main');
+    }
+  };
+  
+  module.exports = { checkUser, secureRoute };
+  
+//   (
+//       checkUser - не даст зайти без регистрации/логина на страницу
+//       secureRoute - не даст зайти на страницу, если вы вошли под юзером
+//   )
+  
